0ce8f194feb7ba65242c9ee60b18d055fa71bd1d
NguyetPhamFuix <nguyetpham@fuixlabs.com>
2023-04-28 09:38:13 +0700

    Add DepositContract and modify the DepositFactoryContract in order to create multi instances of DepositContract by the clone pattern

---
+++ contracts/childs/DepositContract.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+import {DepositItem} from "../library/Structs.sol";
+import {DepositFactoryContract} from "../parents/DepositFactoryContract.sol";
+
+contract DepositContract {
+    address private _sellerAddress;
+    address public tokenAddress;
+    uint256 public dstChainId;
+    uint256 public mintPrice;
+    uint256 public whiteListMintPrice;
+    uint256 public minMintQuantity;
+    uint256 public maxMintQuantity;
+    uint256 public totalSupply;
+    uint256 public deadline;
+    address private _factoryContractAddress;
+
+    constructor(
+        address sellerAddress,
+        address tokenAddress_,
+        uint256 dstChainId_,
+        uint256 mintPrice_,
+        uint256 whiteListMintPrice_,
+        uint256 minMintQuantity_,
+        uint256 maxMintQuantity_,
+        uint256 totalSupply_,
+        uint256 deadline_,
+        address factoryContractAddress
+    ) {
+        _sellerAddress = sellerAddress;
+        tokenAddress = tokenAddress_;
+        dstChainId = dstChainId_;
+        mintPrice = mintPrice_;
+        whiteListMintPrice = whiteListMintPrice_;
+        minMintQuantity = minMintQuantity_;
+        maxMintQuantity = maxMintQuantity_;
+        totalSupply = totalSupply_;
+        deadline = deadline_;
+        _factoryContractAddress = factoryContractAddress;
+    }
+
+    function init(
+        address sellerAddress,
+        address tokenAddress_,
+        uint256 dstChainId_,
+        uint256 mintPrice_,
+        uint256 whiteListMintPrice_,
+        uint256 minMintQuantity_,
+        uint256 maxMintQuantity_,
+        uint256 totalSupply_,
+        uint256 deadline_,
+        address factoryContractAddress
+    ) external {
+        _sellerAddress = sellerAddress;
+        tokenAddress = tokenAddress_;
+        dstChainId = dstChainId_;
+        mintPrice = mintPrice_;
+        whiteListMintPrice = whiteListMintPrice_;
+        minMintQuantity = minMintQuantity_;
+        maxMintQuantity = maxMintQuantity_;
+        totalSupply = totalSupply_;
+        deadline = deadline_;
+        _factoryContractAddress = factoryContractAddress;
+    }
+
+    function mint(
+        DepositItem calldata depositItem,
+        bytes calldata signature,
+        uint256 lzGasFee,
+        bool isNativeToken
+    ) public payable {
+        DepositFactoryContract(_factoryContractAddress).depositTokenByClient(
+            depositItem,
+            signature,
+            lzGasFee,
+            isNativeToken
+        );
+    }
+
+    modifier onlyPermissioned() {
+        require(
+            msg.sender == _sellerAddress ||
+                msg.sender == _factoryContractAddress,
+            "No permission!"
+        );
+        _;
+    }
+
+    function changeMintPrice(uint256 mintPrice_) public onlyPermissioned {
+        mintPrice = mintPrice_;
+    }
+
+    function changeMinMintQuantity(
+        uint256 minMintQuantity_
+    ) public onlyPermissioned {
+        minMintQuantity = minMintQuantity_;
+    }
+
+    function changeMaxMintQuantity(
+        uint256 maxMintQuantity_
+    ) public onlyPermissioned {
+        maxMintQuantity = maxMintQuantity_;
+    }
+
+    function changeDeadline(uint256 deadline_) public onlyPermissioned {
+        deadline = deadline_;
+    }
+
+    function changeTotalSupply(uint256 totalSupply_) public onlyPermissioned {
+        totalSupply = totalSupply_;
+    }
+}
--- contracts/parents/DepositFactoryContract.sol
+++ contracts/parents/DepositFactoryContract.sol
@@ -9,139 +9,293 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
-import { DepositItem } from "../library/Structs.sol";
+
 import "../library/Domain.sol";
 import "@layerzerolabs/solidity-examples/contracts/lzApp/NonblockingLzApp.sol";
 
+import {CloneFactory} from "../library/CloneFactory.sol";
+import {DepositItem} from "../library/Structs.sol";
+import {DepositContract} from "../childs/DepositContract.sol";
+
 import "hardhat/console.sol";
-contract DepositFactoryContract is AccessControl, NonblockingLzApp, EIP712, ReentrancyGuard, Pausable {
-  using SafeERC20 for IERC20;
 
-  event SetAdminWallet(address adminWallet);
-  event SetPlatformFee(uint96 platformFee);
-  event DepositedToken(DepositItem depositItem, uint256 lzGasFee, bool isNativeToken);
-  
-  bytes32 public constant DEPOSIT_ROLE = keccak256("DEPOSIT_ROLE");
+contract DepositFactoryContract is
+    AccessControl,
+    NonblockingLzApp,
+    EIP712,
+    ReentrancyGuard,
+    Pausable,
+    CloneFactory
+{
+    using SafeERC20 for IERC20;
 
-  uint96 public platformFee = 250;   // 2.5% platform fee
-  address private _adminWallet;
-  address private immutable _acceptToken;
-  
-  mapping(address => uint256) private _accountNonces;
+    event SetAdminWallet(address adminWallet);
+    event SetPlatformFee(uint96 platformFee);
+    event DepositedToken(
+        DepositItem depositItem,
+        uint256 lzGasFee,
+        bool isNativeToken
+    );
 
-  constructor(
-    address _layerZeroEndpoint,
-    address acceptToken, 
-    address owner,
-    address adminWallet,
-    address depositRoleAccount
-  ) NonblockingLzApp(_layerZeroEndpoint) EIP712("DepositFactoryContract", "1.0.0") {
-    require(acceptToken != address(0), "AcceptToken cannot be zero address");
-    _acceptToken = acceptToken;
-    _adminWallet = adminWallet;
-    _setupRole(DEFAULT_ADMIN_ROLE, owner);
-    _setupRole(DEPOSIT_ROLE, depositRoleAccount);
-  }
+    bytes32 public constant DEPOSIT_ROLE = keccak256("DEPOSIT_ROLE");
 
-  function supportsInterface(bytes4 interfaceId) public view virtual override(AccessControl) returns (bool) {
-    return super.supportsInterface(interfaceId);
-  }
+    uint96 public platformFee = 250; // 2.5% platform fee
+    address private _adminWallet;
+    address private immutable _acceptToken;
+    address private _masterDepositContract;
 
-  /**
+    mapping(address => uint256) private _accountNonces;
+
+    mapping(address => bool) private _depositContracts;
+
+    constructor(
+        address _layerZeroEndpoint,
+        address acceptToken,
+        address owner,
+        address adminWallet,
+        address depositRoleAccount
+    )
+        NonblockingLzApp(_layerZeroEndpoint)
+        EIP712("DepositFactoryContract", "1.0.0")
+    {
+        require(
+            acceptToken != address(0),
+            "AcceptToken cannot be zero address"
+        );
+        _acceptToken = acceptToken;
+        _adminWallet = adminWallet;
+        _setupRole(DEFAULT_ADMIN_ROLE, owner);
+        _setupRole(DEPOSIT_ROLE, depositRoleAccount);
+    }
+
+    function setMasterDepositContractAddress(
+        address masterDepositContract
+    ) public onlyOwner {
+        _masterDepositContract = masterDepositContract;
+    }
+
+    function createNewDepositContract(
+        address sellerAddress,
+        address tokenAddress,
+        uint256 dstChainId,
+        uint256 mintPrice,
+        uint256 whiteListMintPrice,
+        uint256 minMintQuantity,
+        uint256 maxMintQuantity,
+        uint256 totalSupply,
+        uint256 deadline
+    ) public onlyOwner {
+        DepositContract masterDepositContract = new DepositContract(
+            sellerAddress,
+            tokenAddress,
+            dstChainId,
+            mintPrice,
+            whiteListMintPrice,
+            minMintQuantity,
+            maxMintQuantity,
+            totalSupply,
+            deadline,
+            address(this)
+        );
+        _depositContracts[address(masterDepositContract)] = true;
+        setMasterDepositContractAddress(address(masterDepositContract));
+    }
+
+    function deployClonedDepositContract(
+        address sellerAddress,
+        address tokenAddress,
+        uint256 dstChainId,
+        uint256 mintPrice,
+        uint256 whiteListMintPrice,
+        uint256 minMintQuantity,
+        uint256 maxMintQuantity,
+        uint256 totalSupply,
+        uint256 deadline
+    ) public onlyOwner {
+        address clone = createClone(_masterDepositContract);
+        DepositContract(clone).init(
+            sellerAddress,
+            tokenAddress,
+            dstChainId,
+            mintPrice,
+            whiteListMintPrice,
+            minMintQuantity,
+            maxMintQuantity,
+            totalSupply,
+            deadline,
+            address(this)
+        );
+        _depositContracts[clone] = true;
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    /**
   @dev Setup deposit role
   */
-  function setupDepositRole(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
-      _grantRole(DEPOSIT_ROLE, account);
-  }
-
-  function setAdminWallet(address adminWallet_) external onlyRole(DEFAULT_ADMIN_ROLE)
-  {
-    require (adminWallet_ != address(0), "adminWallet address must not be zero address");
-    _adminWallet = adminWallet_;
-    emit SetAdminWallet(adminWallet_);
-  }
-
-  function setPlatformFee(uint96 platformFee_) external onlyRole(DEFAULT_ADMIN_ROLE)
-  {
-    require (platformFee_ > 0, "platformFee must be greater than zero");
-    require (platformFee_ < 10000, "platformFee must be less than 100%");
-    platformFee = platformFee_;
-    emit SetPlatformFee(platformFee_);
-  }
-
-  function depositTokenByClient(
-    DepositItem calldata depositItem,
-    bytes calldata signature,
-    uint256 lzGasFee,
-    bool isNativeToken
-  ) external nonReentrant whenNotPaused payable {
-    require(msg.sender == tx.origin, "Contract address is not allowed");
-    require(block.timestamp <= depositItem.deadline, "Invalid expiration in deposit");
-    require(depositItem.isMintAvailable, "Mint is not available");
-    if (isNativeToken) {
-      require(msg.value >= lzGasFee + depositItem.mintPrice, "Insufficient native token balances");
-    } else {
-      require(msg.value >= lzGasFee, "Insufficient native token balances");
-    }
-    require(
-      _verify(
-        _hashTypedDataV4(
-          Domain._hashDepositItem(
-            depositItem, 
-            _accountNonces[msg.sender]
-          )),
-      signature
-    ), "Invalid signature");
-    unchecked {
-      ++ _accountNonces[msg.sender];
-    }
-    uint256 platformFeeAmount = _calcFeeAmount(depositItem.mintPrice, platformFee);
-    uint256 userProfit = depositItem.mintPrice - platformFeeAmount;
-    bytes memory encodedPayload = abi.encode(
-      msg.sender,
-      depositItem.mintQuantity,
-      depositItem.sellerAddress
-    );
-    
-    _lzSend(depositItem.dstChainId, encodedPayload, payable(msg.sender), address(0x0), bytes(""), lzGasFee);
-
-    if (isNativeToken) {
-      Address.sendValue(payable(_adminWallet), platformFeeAmount);
-      Address.sendValue(payable(depositItem.sellerAddress), userProfit);
-    } else {
-      IERC20(_acceptToken).safeTransferFrom(msg.sender, _adminWallet, platformFeeAmount);
-      IERC20(_acceptToken).safeTransferFrom(msg.sender, depositItem.sellerAddress, userProfit);
+    function setupDepositRole(
+        address account
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _grantRole(DEPOSIT_ROLE, account);
     }
 
-    emit DepositedToken(depositItem, lzGasFee, isNativeToken);
-  }
-  
-  function estimateFee(uint16 dstChainId_, bool _useZro, bytes calldata _adapterParams) public view returns (uint nativeFee, uint zroFee) {
-    bytes memory encodedPayload = abi.encode(
-      address(0),
-      uint256(0),
-      address(0)
-    );
-    return lzEndpoint.estimateFees(dstChainId_, address(this), encodedPayload, _useZro, _adapterParams);
-  }
+    function setAdminWallet(
+        address adminWallet_
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(
+            adminWallet_ != address(0),
+            "adminWallet address must not be zero address"
+        );
+        _adminWallet = adminWallet_;
+        emit SetAdminWallet(adminWallet_);
+    }
 
-  function _nonblockingLzReceive(uint16, bytes memory, uint64, bytes memory) internal override {}
+    function setPlatformFee(
+        uint96 platformFee_
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(platformFee_ > 0, "platformFee must be greater than zero");
+        require(platformFee_ < 10000, "platformFee must be less than 100%");
+        platformFee = platformFee_;
+        emit SetPlatformFee(platformFee_);
+    }
 
-  function _calcFeeAmount(uint256 amount, uint96 fee) internal pure returns (uint256) {
-    unchecked { return amount * fee / 10000; }
-  }
+    modifier onlyContract() {
+        require(
+            _depositContracts[msg.sender],
+            "Only contracts created by this factory can call the function!"
+        );
+        _;
+    }
 
-  function _verify(bytes32 digest, bytes memory signature)
-  internal view returns (bool)
-  {
-    return hasRole(DEPOSIT_ROLE, ECDSA.recover(digest, signature));
-  }
+    function depositTokenByClient(
+        DepositItem calldata depositItem,
+        bytes calldata signature,
+        uint256 lzGasFee,
+        bool isNativeToken
+    ) external payable nonReentrant whenNotPaused onlyContract {
+        require(msg.sender == tx.origin, "Contract address is not allowed");
+        require(
+            block.timestamp <= depositItem.deadline,
+            "Invalid expiration in deposit"
+        );
+        require(depositItem.isMintAvailable, "Mint is not available");
+        if (isNativeToken) {
+            require(
+                msg.value >= lzGasFee + depositItem.mintPrice,
+                "Insufficient native token balances"
+            );
+        } else {
+            require(
+                msg.value >= lzGasFee,
+                "Insufficient native token balances"
+            );
+        }
+        require(
+            _verify(
+                _hashTypedDataV4(
+                    Domain._hashDepositItem(
+                        depositItem,
+                        _accountNonces[msg.sender]
+                    )
+                ),
+                signature
+            ),
+            "Invalid signature"
+        );
+        unchecked {
+            ++_accountNonces[msg.sender];
+        }
+        uint256 platformFeeAmount = _calcFeeAmount(
+            depositItem.mintPrice,
+            platformFee
+        );
+        uint256 userProfit = depositItem.mintPrice - platformFeeAmount;
+        bytes memory encodedPayload = abi.encode(
+            msg.sender,
+            depositItem.mintQuantity,
+            depositItem.sellerAddress
+        );
 
-  function pause() public virtual onlyRole(DEFAULT_ADMIN_ROLE) {
-      _pause();
-  }
+        _lzSend(
+            depositItem.dstChainId,
+            encodedPayload,
+            payable(msg.sender),
+            address(0x0),
+            bytes(""),
+            lzGasFee
+        );
 
-  function unpause() public virtual onlyRole(DEFAULT_ADMIN_ROLE) {
-      _unpause();
-  }
+        if (isNativeToken) {
+            Address.sendValue(payable(_adminWallet), platformFeeAmount);
+            Address.sendValue(payable(depositItem.sellerAddress), userProfit);
+        } else {
+            IERC20(_acceptToken).safeTransferFrom(
+                msg.sender,
+                _adminWallet,
+                platformFeeAmount
+            );
+            IERC20(_acceptToken).safeTransferFrom(
+                msg.sender,
+                depositItem.sellerAddress,
+                userProfit
+            );
+        }
+
+        emit DepositedToken(depositItem, lzGasFee, isNativeToken);
+    }
+
+    function estimateFee(
+        uint16 dstChainId_,
+        bool _useZro,
+        bytes calldata _adapterParams
+    ) public view returns (uint nativeFee, uint zroFee) {
+        bytes memory encodedPayload = abi.encode(
+            address(0),
+            uint256(0),
+            address(0)
+        );
+        return
+            lzEndpoint.estimateFees(
+                dstChainId_,
+                address(this),
+                encodedPayload,
+                _useZro,
+                _adapterParams
+            );
+    }
+
+    function _nonblockingLzReceive(
+        uint16,
+        bytes memory,
+        uint64,
+        bytes memory
+    ) internal override {}
+
+    function _calcFeeAmount(
+        uint256 amount,
+        uint96 fee
+    ) internal pure returns (uint256) {
+        unchecked {
+            return (amount * fee) / 10000;
+        }
+    }
+
+    function _verify(
+        bytes32 digest,
+        bytes memory signature
+    ) internal view returns (bool) {
+        return hasRole(DEPOSIT_ROLE, ECDSA.recover(digest, signature));
+    }
+
+    function pause() public virtual onlyRole(DEFAULT_ADMIN_ROLE) {
+        _pause();
+    }
+
+    function unpause() public virtual onlyRole(DEFAULT_ADMIN_ROLE) {
+        _unpause();
+    }
 }
